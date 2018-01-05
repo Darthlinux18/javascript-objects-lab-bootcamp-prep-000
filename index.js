7bfc8d5b532954d8a8d67dec164564ba3b1ba08f
craig sperlazza <craig.sperlazza@gmail.com>
2018-01-05 21:08:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,8 +10,17 @@ function destructivelyUpdateObjectWithKeyAndValue (recipes, key, value) {
 }
 
 function deleteFromObjectByKey (object, key) {
+<<<<<<< HEAD
   var object2 = Object.assign ({}, object);
   delete(object2[key]);
+=======
+<<<<<<< HEAD
+  var object2 = Object.assign({}, object)
+=======
+  var object2 = Object.assign({}, obj)
+>>>>>>> c2196caaffa6d2126f4fac6d95380d4cc73396cb
+  delete object2[key]
+>>>>>>> 5a69f5232dad92418ef390e1da2c4402195c13c6
   return object2
 }
 
