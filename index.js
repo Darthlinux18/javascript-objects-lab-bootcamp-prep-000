adee75c6ec703f7faf1e470a89ef5783a221c6a7
craig sperlazza <craig.sperlazza@gmail.com>
2018-01-05 21:30:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ function destructivelyUpdateObjectWithKeyAndValue (object, key, value) {
   return object
 }
 
+function
+
 function destructivelyDeleteFromObjectByKey (object, key) {
-  delete object.key;
+  delete object[key];
   return object
 }
